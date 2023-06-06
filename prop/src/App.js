0bd226e682508931e0d48b12f50a3ee4b942b51d
Mariem3493 <127346106+Mariem3493@users.noreply.github.com>
2023-06-06 21:12:18 +0200

    Update App.js

--- prop/src/App.js
+++ prop/src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+/*importation du stylesheet du framework bootstrap*/
 import 'bootstrap/dist/css/bootstrap.min.css';
+/*importation du composant ContainerSyst*/
 import ContainerSyst from './Component/ContainerSyst';
+/*Function App qui fait appel au composant ContainerSyst*/
 function App() {
   return (
    <div>
