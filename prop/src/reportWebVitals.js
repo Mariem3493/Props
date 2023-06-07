16a3caa82e43195970552da7c71c0747aa79aefe
Mariem3493 <127346106+Mariem3493@users.noreply.github.com>
2023-06-07 21:02:38 +0200

    Update reportWebVitals.js

--- prop/src/reportWebVitals.js
+++ prop/src/reportWebVitals.js
@@ -1,3 +1,4 @@
+/*Fonction pour la mesure de la performance de l'application*/
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
