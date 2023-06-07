b49c09d141a6dc3c80cdf631d0474f53dc4cd00b
Mariem3493 <127346106+Mariem3493@users.noreply.github.com>
2023-06-07 20:57:04 +0200

    Update index.js

--- prop/src/index.js
+++ prop/src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 import App from './App';
 /*importation de la fonction reportWebVitals*/
 import reportWebVitals from './reportWebVitals';
-
+/*initialisation de l'application react et render de l'application*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
